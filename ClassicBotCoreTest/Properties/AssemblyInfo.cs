6cdab57d6899ac8c3af5334b447f52295b27ae34
BjorN64 <BJORN@.(none)>
2012-11-24 08:05:30 +1100

    Updated AssemblyInfo files.

--- ClassicBotCoreTest/Properties/AssemblyInfo.cs
+++ ClassicBotCoreTest/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibClassicBot GUI")]
+[assembly: AssemblyTitle("LibClassicBot CLI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibClassicBotTest")]
+[assembly: AssemblyProduct("LibClassicBot CLI")]
 [assembly: AssemblyCopyright("Copyright 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- LibClassicBot GUI/Properties/AssemblyInfo.cs
+++ LibClassicBot GUI/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("One")]
+[assembly: AssemblyTitle("LibClassicBot GUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("One")]
+[assembly: AssemblyProduct("LibClassicBot GUI")]
 [assembly: AssemblyCopyright("Copyright 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
